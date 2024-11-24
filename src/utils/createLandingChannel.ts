cfbc476b6b38995625357362f12dd6e3fd156158
newn0w <samuelpetersonacs@gmail.com>
2024-11-24 16:14:06 -0500

    Refresh landing channel on startup

--- src/utils/createLandingChannel.ts
+++ src/utils/createLandingChannel.ts
@@ -1,41 +1,47 @@
-import {Guild, TextChannel, PermissionsBitField, ChannelType} from "discord.js";
+import {Guild, TextChannel, PermissionsBitField, ChannelType, Message} from "discord.js";
 
 const LANDING_CHANNEL_NAME = "verify-here";
 
 export const createLandingChannel = async (guild: Guild): Promise<TextChannel | null> => {
-// Check if the channel already exists
-    const existingChannel = guild.channels.cache.find(
-        (channel) => channel.name === LANDING_CHANNEL_NAME
-    );
-
-    if (existingChannel) {
-        return null;
-    }
-
     if (!guild.members.me?.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
         console.error("Bot does not have sufficient permissions!");
         return null;
     }
 
-    // If the channel does not exist, create a new one
-    try {
-        const newChannel = await guild.channels.create({
-            name: LANDING_CHANNEL_NAME,
-            type: ChannelType.GuildText,
-            topic: 'Please verify your email to gain access.',
-            reason: 'Creating landing channel for email verification',
-            permissionOverwrites: [
-                {
-                    id: guild.id,
-                    allow: [PermissionsBitField.Flags.ViewChannel], // TODO: Give access to "verified" role
-                    deny: [PermissionsBitField.Flags.SendMessages]
-                },
-            ],
-        });
+    // Check if the channel already exists
+    const existingChannel = guild.channels.cache.find(
+        (channel) => channel.name === LANDING_CHANNEL_NAME
+    );
 
-        return newChannel
-    } catch (error) {
-        console.error('Error creating the landing channel:', error);
-        return null;
+    if (!existingChannel || existingChannel.type !== ChannelType.GuildText) {
+        // If the channel does not exist, create a new one
+        try {
+            const newChannel = await guild.channels.create({
+                name: LANDING_CHANNEL_NAME,
+                type: ChannelType.GuildText,
+                topic: 'Please verify your email to gain access.',
+                reason: 'Creating landing channel for email verification',
+                permissionOverwrites: [
+                    {
+                        id: guild.id,
+                        allow: [PermissionsBitField.Flags.ViewChannel], // TODO: Give access to "verified" role
+                        deny: [PermissionsBitField.Flags.SendMessages]
+                    },
+                ],
+            }) as TextChannel;
+
+            return newChannel;
+        } catch (error) {
+            console.error('Error creating the landing channel:', error);
+            return null;
+        }
     }
+
+    // If channel already exists, clear any pre-existing messages and return the existing channel
+    const existingMessages = await existingChannel.messages.fetch({ limit: 100 });
+    // await existingChannel.bulkDelete(existingMessages); TODO: Ask Dave if this msgs will never be >2wks old
+    for (const message of existingMessages.values()) {
+        message.delete();
+    }
+    return existingChannel;
 };
